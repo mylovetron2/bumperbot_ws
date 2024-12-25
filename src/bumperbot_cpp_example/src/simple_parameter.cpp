3a1afbbfa79411e69ba8812f471b0f6d2a17ec3f
mylovetron2 <mystore2018page1@gmail.com>
2024-12-25 15:33:34 +0700

    25/12/24

--- src/bumperbot_cpp_example/src/simple_parameter.cpp
+++ src/bumperbot_cpp_example/src/simple_parameter.cpp
@@ -1,5 +1,8 @@
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
+//#include <std_msgs/msg/string.hpp>
+#include <rcl_interfaces/msg/set_parameters_result.hpp>
+#include <string>
+#include <vector>
 
 class SimpleParameter: public rclcpp::Node
 {
@@ -8,11 +11,38 @@ class SimpleParameter: public rclcpp::Node
             declare_parameter<int>("simple_int_param",28);
             declare_parameter<std::string>("simple_string_param","Antonio");
 
-            add_on_set_parameters_callback(std::bind(SimpleParameter::paramChangeCallback,this, _1));
+            param_callback_handle_= add_on_set_parameters_callback(std::bind(&SimpleParameter::paramChangeCallback,this, _1));
         }
     private:
-        OnSetParameterCallbackHandle::SharedPtr param_callback_handle_;
-        Test oK
     
-        "Im OK "
+       OnSetParametersCallbackHandle::SharedPtr param_callback_handle_;
+
+       
+       rcl_interfaces::msg::SetParametersResult paramChangeCallback(const std::vector<rclcpp::Parameter> &parameters)
+       {
+        rcl_interfaces::msg::SetParametersResult result;
+        for(const auto& param:parameters){
+            if(param.get_name()=="simple_int_param" && param.get_type()==rclcpp::ParameterType::PARAMETER_INTEGER)
+            {
+                RCLCPP_INFO_STREAM(get_logger(),"Param simple_int_param changed! New value is: " <<param.as_int());
+                result.successful=true;
+            }
+            if(param.get_name()=="simple_string_param" && param.get_type()==rclcpp::ParameterType::PARAMETER_STRING)
+            {
+                RCLCPP_INFO_STREAM(get_logger(),"Param simple_int_param changed! New value is: " <<param.as_int());
+                result.successful=true;
+            }
+
+        }
+       }
+        
+
+};
+
+int main(int argc,char *argv[]){
+    rclcpp::init(argc,argv);
+    auto node=std::make_shared<SimpleParameter>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
